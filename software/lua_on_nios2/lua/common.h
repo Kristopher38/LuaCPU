13fe0da7d84957a04798ab022c555e16f06ed68c
Kristopher38 <kristopher38@wp.pl>
2022-02-15 20:06:25 +0100

    Added stricter register syncing

--- software/lua_on_nios2/lua/common.h
+++ software/lua_on_nios2/lua/common.h
@@ -5,6 +5,14 @@
     #define CUSTOM_LUA
 #endif
 
+#define SYNC_REGS(ci, base) { \
+    for (StkId slot = base; slot < ci->top; slot++) \
+    { \
+        ALT_CI_LUA_STOREVAL(slot, slot->value_); \
+        ALT_CI_LUA_STORETT(slot, slot->tt_); \
+    } \
+}
+
 #ifdef __NIOS2__
 	#include "system.h"
 	#define ALT_CI_LUA_EXEC(A,B) __builtin_custom_inpp(ALT_CI_LUA_CPU_0_N+0,(A),(B))
@@ -13,7 +21,7 @@
     #define ALT_CI_LUA_STOREVALP(reg, val) __builtin_custom_npp(ALT_CI_LUA_CPU_0_N+(1&ALT_CI_LUA_CPU_0_N_MASK),(void*)(reg),(void*)(val))
     #define ALT_CI_LUA_STOREVAL(reg, val) __asm__("custom 1,zero,%0,%1" :: "r" (reg), "r" (val)) 
 	#define ALT_CI_LUA_STORETT(reg, tt) __builtin_custom_npi(ALT_CI_LUA_CPU_0_N+(2&ALT_CI_LUA_CPU_0_N_MASK),(void*)(reg),(int)(tt))
-    #define ALT_CI_LUA_STOREBASE(base) __builtin_custom_np(ALT_CI_LUA_CPU_0_N+(3&ALT_CI_LUA_CPU_0_N_MASK),(void*)(base))
+    #define ALT_CI_LUA_STOREBASE(base) { __builtin_custom_np(ALT_CI_LUA_CPU_0_N+(3&ALT_CI_LUA_CPU_0_N_MASK),(void*)(base)); SYNC_REGS(L->ci, base); }
 #elif VERILATOR_SIM
     #include "cpusim.h"
 	#define ALT_CI_LUA_EXEC(A,B) luacpu_simulate(A, B);
@@ -22,7 +30,7 @@
     #define ALT_CI_LUA_STOREVALP(reg, val) luacpu_storeval((void*)reg, val)
     #define ALT_CI_LUA_STOREVAL(reg, val) luacpu_storeval((void*)reg, val)
 	#define ALT_CI_LUA_STORETT(reg, tt) luacpu_storett((void*)reg, tt)
-    #define ALT_CI_LUA_STOREBASE(base) luacpu_storebase((void*)base)
+    #define ALT_CI_LUA_STOREBASE(base) { luacpu_storebase((void*)base); SYNC_REGS(L->ci, base); }
 #else
 	#define ALT_CI_LUA_EXEC(A,B) (void)0
 	#define ALT_CI_LUA_STOREVALI(reg, val) (void)0 
